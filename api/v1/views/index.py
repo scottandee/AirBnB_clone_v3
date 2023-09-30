9e1bba7239c37c4789f953beee64d16c8540d05d
Olayinkascott Andee <andeeolayinkascott@gmail.com>
2023-09-30 08:48:30 +0100

    Revert previous commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,7 +22,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats")
+@app_views.route("/stats", strict_slashes=False)
 def stats():
     """Return objects and their number in JSON"""
     stats = {}
