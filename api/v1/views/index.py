3ca692249354e563d2d48db43284517b9f722695
TebogoLegoabe <tebogolegoabe5@gmail.com>
2023-09-29 17:50:01 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,3 +20,15 @@ classes = {"amenity": Amenity, "city": City,
 def status():
     """Returns the status as JSON"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
